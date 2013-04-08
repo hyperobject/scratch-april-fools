788f27189d15e236dbd50d326995a0487ebe7bd2
Technoboy10 <cchudson98@gmail.com>
2013-04-08 16:09:58 -0600

    Update userscript.js
    fix bugs

--- userscript.js
+++ userscript.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name           April Fool's Day
-<<<<<<< HEAD
+//<<<<<<< HEAD
 // @version        1.0
 // @namespace      
 // @description    Re-implements the censors that the ST added for April Fool's Day in past years
 // @include        http://scratch.mit.edu/forums/*
 // ==/UserScript==
 
-=======
+//=======
 // @version        1.1
 // @namespace      
 // @description    Re-implements the April Fool's Day pranks that the ST added for April Fool's Day in past years
@@ -15,7 +15,7 @@
 // ==/UserScript==
 
 //Censors
->>>>>>> Adds the Neigh Logo
+//>>>>>>> Adds the Neigh Logo
 var censors = {};
 censors["pi "] = '3.14159265... ';
 censors["you "] = "pikachu ";
@@ -41,16 +41,16 @@ censors["th3 "] = "(Pico rules - don't get smart!!) ";
 censors["4nd "] = "(Bloop - very tricky!!) ";
 censors["th[b][/b]e "] = "(Pico still rules!!) ";
 censors["(Pico rules!!)ad"] = 'thead';
-<<<<<<< HEAD
+//<<<<<<< HEAD
 var target = document.getElementById("punwrap");
 var myText = target.innerHTML;
-=======
+//=======
 var target = document.body;
 var myText = target.innerHTML;
 //Replaces the Scratch Logo with the Neigh Logo.
 myText = myText.replace('<a href="/">Scratch</a>', "<img src='http://i.imgur.com/Apomb.png' />");
 myText = myText.replace('<a href="/"></a>', "<img src='http://i.imgur.com/Apomb.png' />");
->>>>>>> Adds the Neigh Logo
+//>>>>>>> Adds the Neigh Logo
 for (censor in censors) {
   myText = myText.replace(makeregex(censor, "g"), censors[censor]);
 }
