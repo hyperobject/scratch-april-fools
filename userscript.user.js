5f87c23cc141f5b83d86922878c968eb807fc487
Garrison Taylor <garrison@imac.home>
2013-04-14 09:03:57 -0400

    Updating the userscript
    Updates the script to the newest version and hopefully makes it work
    for the whole site, instead of just the forum.

--- userscript.user.js
+++ userscript.user.js
@@ -1,16 +1,10 @@
 // ==UserScript==
 // @name           April Fool's Day
-// @version        1.0
-// @namespace      
-// @description    Re-implements the censors that the ST added for April Fool's Day in past years
-// @include        http://scratch.mit.edu/forums/*
-// ==/UserScript==
-
-=======
 // @version        1.1
 // @namespace      
-// @description    Re-implements the April Fool's Day pranks that the ST added for April Fool's Day in past years
+// @description    Re-implements some of the April Fool's Day pranks that the ST added for April Fool's Day in past years
 // @include        http://scratch.mit.edu/*
+// @include        http://*.scratch.mit.edu/*
 // ==/UserScript==
 
 //Censors
@@ -33,18 +27,13 @@ censors["computer"] = "toaster";
 censors["only"] = "ponly";
 censors["Scratch"] = "Neigh";
 censors["Ponys"] = "Ponies";
-censors["and "] = "(Bloop!!) ";
-censors["the "] = "(Pico rules!!) ";
+censors["the "] = "(Bloop!!) ";
+censors["and "] = "(Pico rules!!) ";
 censors["th3 "] = "(Pico rules - don't get smart!!) ";
 censors["4nd "] = "(Bloop - very tricky!!) ";
 censors["th[b][/b]e "] = "(Pico still rules!!) ";
-censors["(Pico rules!!)ad"] = 'thead';
-var target = document.getElementById("punwrap");
-var myText = target.innerHTML;
-=======
 var target = document.body;
 var myText = target.innerHTML;
-//Replaces the Scratch Logo with the Neigh Logo.
 myText = myText.replace('<a href="/">Scratch</a>', "<img src='http://i.imgur.com/Apomb.png' />");
 myText = myText.replace('<a href="/"></a>', "<img src='http://i.imgur.com/Apomb.png' />");
 for (censor in censors) {
